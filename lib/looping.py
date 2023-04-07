ef8298157e1dc673e5b138b0a7dacf3b70d167b2
Holden Gerner <hgerner19@gmail.com>
2023-04-07 09:44:35 -0600

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i >= 0:
+        if i == 0:
+            print("Happy New Year!")
+        else:
+            print(i)
+            
+        i -= 1
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    square_list = list()
+    for num in int_list:
+        square_int = num * num
+        square_list.append(square_int)
+    return square_list
     pass
 
 def fizzbuzz():
-    # code goes here!
+    i = 1
+    while(i <= 100):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                print("FizzBuzz")
+            else:
+                print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else: 
+            print(i)
+        i += 1
+    
     pass
